1a8bf34972eef749089e9a081184d5f4e8add405
vadym <vadym.spitsyn@gmail.com>
2025-03-31 05:11:46 +0200

    Added and tested simple ui android test

--- src/test/java/com/solvd/mobile/MobileAndroidTest.java
+++ src/test/java/com/solvd/mobile/MobileAndroidTest.java
@@ -1,7 +1,7 @@
 package com.solvd.mobile;
 
-import com.solvd.mobile.common.LoginPageBase;
-import com.solvd.mobile.common.WelcomePageBase;
+import com.solvd.mobile.android.ClockAlarmPage;
+import com.solvd.mobile.android.ClockHomePage;
 import com.zebrunner.carina.core.IAbstractTest;
 import com.zebrunner.carina.utils.mobile.IMobileUtils;
 import org.testng.annotations.Test;
@@ -9,9 +9,19 @@ import org.testng.annotations.Test;
 public class MobileAndroidTest implements IAbstractTest, IMobileUtils {
 
     @Test()
-    public void testUIElements() {
-        WelcomePageBase welcomePage = initPage(getDriver(), WelcomePageBase.class);
-        LoginPageBase loginPage = welcomePage.clickNextBtn();
+    public void testUIElementsCreate() {
+        ClockHomePage clockHomePage = new ClockHomePage(getDriver());
+        ClockAlarmPage clockAlarmPage = clockHomePage.goToAlarmPage();
+
+        clockAlarmPage.createAlarm();
+    }
+
+    @Test()
+    public void testUIElementsDelete() {
+        ClockHomePage clockHomePage = new ClockHomePage(getDriver());
+        ClockAlarmPage clockAlarmPage = clockHomePage.goToAlarmPage();
+
+        clockAlarmPage.deleteFirstAlarm();
     }
 
 }
