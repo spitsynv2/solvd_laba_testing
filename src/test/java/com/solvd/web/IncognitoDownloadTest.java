95bb4dcc48bbd6ae373e7826845c8981c8a27e2e
Vadym Spitsyn <vspitsyn@solvd.com>
2025-10-16 12:21:57 +0200

    Small refactor

--- src/test/java/com/solvd/web/IncognitoDownloadTest.java
+++ src/test/java/com/solvd/web/IncognitoDownloadTest.java
@@ -1,5 +1,8 @@
 package com.solvd.web;
 
+import com.zebrunner.agent.core.webdriver.RemoteWebDriverFactory;
+import com.zebrunner.carina.core.AbstractTest;
+import com.solvd.web.gui.pages.PexelsMainPage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
@@ -17,11 +20,6 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-
-import com.zebrunner.agent.core.webdriver.RemoteWebDriverFactory;
-import com.solvd.web.gui.pages.PexelsMainPage;
-import com.zebrunner.carina.core.AbstractTest;
-
 
 import com.zebrunner.carina.utils.R;
 
