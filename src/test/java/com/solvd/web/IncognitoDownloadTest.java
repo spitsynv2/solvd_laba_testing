87e427b8bcaea64a0033c5d5e8203ae41fff2351
Vadym Spitsyn <vspitsyn@solvd.com>
2025-10-16 18:45:06 +0200

    Changed download path

--- src/test/java/com/solvd/web/IncognitoDownloadTest.java
+++ src/test/java/com/solvd/web/IncognitoDownloadTest.java
@@ -135,7 +135,7 @@ public class IncognitoDownloadTest extends AbstractTest {
             Pattern pattern = Pattern.compile("href=\"([^\"]+)\"");
             Matcher matcher = pattern.matcher(html);
 
-            Path localDownloadDir = Paths.get("/home/selenium/Downloads");
+            Path localDownloadDir = Paths.get("tmp/downloads");
             Files.createDirectories(localDownloadDir);
 
             int fileCount = 0;
