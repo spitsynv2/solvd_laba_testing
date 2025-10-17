1daf01421ef705ecebd1b3aa1faf95825938197f
Vadym Spitsyn <vspitsyn@solvd.com>
2025-10-17 10:54:10 +0200

    Change save dir

--- src/test/java/com/solvd/web/IncognitoDownloadTest.java
+++ src/test/java/com/solvd/web/IncognitoDownloadTest.java
@@ -135,7 +135,7 @@ public class IncognitoDownloadTest extends AbstractTest {
             Pattern pattern = Pattern.compile("href=\"([^\"]+)\"");
             Matcher matcher = pattern.matcher(html);
 
-            Path localDownloadDir = Paths.get("tmp/downloads");
+            Path localDownloadDir = Paths.get("tmp/log");
             Files.createDirectories(localDownloadDir);
 
             int fileCount = 0;
