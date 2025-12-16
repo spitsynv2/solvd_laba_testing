91612b9fb25d9b735b3f59cb289af601673f731e
Vadym Spitsyn <vspitsyn@solvd.com>
2025-12-16 16:53:43 +0100

    Idle test

--- src/test/java/com/solvd/web/EbayWebDesktopTests.java
+++ src/test/java/com/solvd/web/EbayWebDesktopTests.java
@@ -121,6 +121,14 @@ public class EbayWebDesktopTests implements IAbstractTest, IAbstractDataProvider
         ebayHomePage.open();
         logCurrentDriverInfoUnwrapped();
 
+        LOGGER.info("============= STARTING IDLE TEST ===============");
+        try {
+            Thread.sleep(420000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        LOGGER.info("============= IDLE TEST FINISHED ===============");
+
         CategoryPageBase electronicsPage = ebayHomePage.selectCategory("Electronics");
         ComputersTabletsNetworkPageBase computersTabletsNetworkPage = electronicsPage.openComputersTabletsNetworkPage();
 
@@ -129,6 +137,8 @@ public class EbayWebDesktopTests implements IAbstractTest, IAbstractDataProvider
         String expectedItemName = itemPageBase.getItemName();
 
         Assert.assertEquals(limitedTimeDealItemName, expectedItemName);
+
+        LOGGER.info("============= TEST SURVIVED IDLE ===============");
     }
 
     @DataProvider(name = "DP1", parallel = true) // keep parallel to observe scheduling/idle gaps
